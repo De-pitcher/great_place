3897ef61be35eee4bd93f41851844d862c0afb7a
De-pitcher <emmanwa000@gmail.com>
2023-01-03 15:28:17 +0100

    ADD: storing and fetching data with SQLite

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,35 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart';
+
+const userDB = 'places.db';
+const userPlaceTable = 'user_places';
+const userId = 'id';
+const userTitle = 'title';
+const userPlaceImage = 'image';
+
+class DBHelper {
+  static Future<Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(path.join(dbPath, userDB), onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE $userPlaceTable ($userId TEXT PRIMARY KEY, $userTitle TEXT, $userPlaceImage TEXT)',
+      );
+    }, version: 1);
+  }
+
+  static Future<void> insert(String table, Map<String, Object?> data) async {
+    final db = await DBHelper.database();
+    // print(data);
+    await db.insert(
+      table,
+      data,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
+  }
+}
--- lib/providers/great_place.dart
+++ lib/providers/great_place.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -10,6 +11,7 @@ class GreatPlaces with ChangeNotifier {
   List<Place> get items => [..._items];
 
   void addPlace(String pickedTitle, File pickedImage) {
+    print(pickedImage);
     final newPlace = Place(
       id: DateTime.now().toString(),
       title: pickedTitle,
@@ -17,6 +19,28 @@ class GreatPlaces with ChangeNotifier {
       image: pickedImage,
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert(
+      userPlaceTable,
+      {
+        userId: newPlace.id,
+        userTitle: newPlace.title,
+        userPlaceImage: newPlace.image.path, 
+      },
+    );
+  }
+
+  Future<void> fetchPlaces() async {
+    final dataList = await DBHelper.getData(userPlaceTable);
+    _items = dataList
+        .map(
+          (item) => Place(
+              id: item[userId],
+              title: item[userTitle],
+              image: File(item[userPlaceImage]),
+              location: null),
+        )
+        .toList();
     notifyListeners();
   }
 }
